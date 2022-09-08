c6d9725640d7d0ee929a0a98ea0bbc143e87634e
Maksim Batischev <Maksim.Batischev@raiffeisen.ru>
2022-09-08 14:59:12 +0300

    Sql insert script has been added to LoanApplicationRepositoryIT.

--- src/test/java/io/github/flexibletech/offering/infrastructure/persistence/LoanApplicationRepositoryIT.java
+++ src/test/java/io/github/flexibletech/offering/infrastructure/persistence/LoanApplicationRepositoryIT.java
@@ -1,9 +1,8 @@
 package io.github.flexibletech.offering.infrastructure.persistence;
 
+import io.github.flexibletech.offering.TestValues;
 import io.github.flexibletech.offering.domain.LoanApplicationRepository;
-import io.github.flexibletech.offering.domain.factory.TestLoanApplicationFactory;
 import io.github.flexibletech.offering.infrastructure.config.ApplicationConfig;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,9 +11,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDate;
 
 @ActiveProfiles("it")
 @DataJpaTest(includeFilters = @ComponentScan.Filter(
@@ -27,14 +25,10 @@ public class LoanApplicationRepositoryIT {
 
     @Test
     @Transactional
+    @Sql("/sql/insert_loan_application.sql")
     public void shouldFindLoanApplicationById() {
-        var loanApplication = loanApplicationRepository.save(TestLoanApplicationFactory.newLoanApplicationWithoutId());
+        var loanApplication = loanApplicationRepository.findById(TestValues.LOAN_APPLICATION_ID).orElse(null);
 
-        var foundLoanApplication = loanApplicationRepository.findById(loanApplication.getId()).orElse(null);
-
-        Assertions.assertNotNull(foundLoanApplication);
-        Assertions.assertTrue(StringUtils.isNoneBlank(foundLoanApplication.getId()));
-        Assertions.assertEquals(foundLoanApplication.getCreatedAt().toLocalDate(), LocalDate.now());
-        Assertions.assertEquals(foundLoanApplication.getUpdatedAt().toLocalDate(), LocalDate.now());
+        Assertions.assertNotNull(loanApplication);
     }
 }
